05bfb90d69d251a3edc354579ae7fae927a1f0c8
Roman Egunkov <romanegunkov@gmail.com>
2016-11-09 00:56:00 +0300

    Initial starman.

---
+++ haskell/futurelearn/starman.hs
@@ -0,0 +1,29 @@
+-- starman.hs
+
+check :: String -> String -> Char -> (Bool, String)
+check word display c
+    = (c `elem` word, [if x==c
+        then c
+        else y | (x,y) <- zip word display])
+
+turn :: String -> String -> Int -> IO ()
+turn word display n =
+    do putStrLn ("Word: " ++ display ++ "    " ++
+                 "Tries: " ++ take n (repeat '*'))
+       if n==0
+         then putStrLn "You lose."
+         else if word==display
+                then putStrLn "You win!"
+                else mkguess word display n
+
+mkguess :: String -> String -> Int -> IO ()
+mkguess word display n =
+    do putStr "Enter your guess: "
+       q <- getLine
+       putStrLn ""
+       let (correct, display') = check word display ((q ++ "_")!!0)
+       let n' = if correct then n else n-1
+       turn word display' n'
+
+starman :: String -> Int -> IO ()
+starman word n = turn word ['-' | x <- word] n
