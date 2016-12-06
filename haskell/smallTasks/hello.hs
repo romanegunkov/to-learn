039d07cd8dbf4f57e0168b60ad2b61e0d61f13fd
Roman Egunkov <romanegunkov@gmail.com>
2016-12-06 22:25:24 +0300

    mv scripts/hello.hs smallTask/hello.hs Add comment into one.

--- haskell/smallTasks/hello.hs
+++ haskell/smallTasks/hello.hs
@@ -1,5 +1,9 @@
 #!/usr/bin/env stack
 -- stack runghc --install-ghc
+
+-- Scripting example in Haskell. Using Stack.
+-- https://www.youtube.com/watch?v=UVek-DEc_pQ
+
 main = do
     putStrLn "Hello, World!"
     putStrLn "Hi!"
